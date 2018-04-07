50237a1f17fe0b285027dcbd31071d86a2b81421
Krzysztof Trzewiczek <krzysztof@singup.pl>
2018-04-07 09:56:05 +0200

    [Closes #2] Basic OSC playground

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Working just fine!')
+import dgram from 'dgram'
+import osc from 'osc-min'
+
+const server = dgram.createSocket('udp4')
+
+const send = (address, args) => {
+  console.log(`>>> [sending] ${address} ${args}`)
+
+  server.send(
+    osc.toBuffer({ address, args }),
+    7400,
+    'localhost',
+    (err) => err
+      ? console.log(`!!! ${err}`)
+      : console.log(`>>> [success] ${address} ${args}`)
+  )
+}
+
+send('/test', [ 3, 6.28, 'Test Message' ])
+send('/csv', '3,6.28,8')
